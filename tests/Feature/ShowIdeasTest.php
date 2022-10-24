195b2a109827c0b61dab2dd66e5359f9688c29e1
EXT84173 <matias.orcajo@claro.com.ar>
2022-10-24 20:59:47 -0300

    Final of episode 16

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -2,27 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use App\Models\Idea;
+use App\Models\Status;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ShowIdeasTest extends TestCase
 {
     use RefreshDatabase;
+    use DatabaseMigrations;
 
     /** @test */
     public function list_of_ideas_shows_on_main_page()
     {
 
-        $ideaOne = Idea::factory()->create([
-            'title' => "My First Title",
-            'description' => "Description of my first title"
-        ]);
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
 
+        $statusOpen = Status::factory()->create(['name' => 'Open']);
+        $statusConsidering = Status::factory()->create(['name' => 'Considering']);
+
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'description' => 'Description of my first idea',
+        ]);
         $ideaTwo = Idea::factory()->create([
-            'title' => "My Second Title",
-            'description' => "Description of my second title"
+            'title' => 'My Second Idea',
+            'category_id' => $categoryTwo->id,
+            'status_id' => $statusConsidering->id,
+            'description' => 'Description of my second idea',
         ]);
 
         $response = $this->get(route('idea.index'));
@@ -37,9 +50,14 @@ class ShowIdeasTest extends TestCase
     /** @test */
     public function single_idea_shows_correctly_on_the_show_page()
     {
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $statusOpen = Status::factory()->create(['name' => 'Open']);
+
         $idea = Idea::factory()->create([
-            'title' => "My First Title",
-            'description' => "Description of my first title"
+            'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'description' => 'Description of my first idea',
         ]);
 
         $response = $this->get(route('idea.show', $idea));
@@ -52,12 +70,23 @@ class ShowIdeasTest extends TestCase
     /** @test */
     public function ideas_with_same_title_has_different_slugs()
     {
-        $ideaOne = Idea::factory()->create([
-            'title' => "My First Title",
-        ]);
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
 
+        $statusOpen = Status::factory()->create(['name' => 'Open']);
+        $statusConsidering = Status::factory()->create(['name' => 'Considering']);
+
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'description' => 'Description of my first idea',
+        ]);
         $ideaTwo = Idea::factory()->create([
-            'title' => "My First Title",
+            'title' => 'My Second Idea',
+            'category_id' => $categoryTwo->id,
+            'status_id' => $statusConsidering->id,
+            'description' => 'Description of my second idea',
         ]);
 
         $this->assertTrue($ideaOne->slug !== $ideaTwo->slug);
